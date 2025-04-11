68b436e37aa0e4af235a3f2dd86bed9683aa7ae5
张淋 <linus.zhang@wearlab.tech>
2025-04-11 11:07:05 +0800

    V1.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup,find_packages
 setup(name='bioencoder',
-      version='beta',
+      version='1.0.0',
       description='A tool to encode AA to calculable encodings',
       author='iceshadows',
       author_email='adron_iceshadows@hotmail.com',
       zip_safe=False,
-      packages=find_packages(),
+      packages=['bioencoder'],
       include_package_data=True,
       package_data={
         "": [".data/*.txt"]
