1921b8d987763959ee7788431e6f0e8998db89f9
张淋 <linus.zhang@wearlab.tech>
2025-04-11 11:13:02 +0800

    V1.0.0- add deps

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup,find_packages
-setup(name='bioencoder',
-      version='1.0.0',
-      description='A tool to encode AA to calculable encodings',
-      author='iceshadows',
-      author_email='adron_iceshadows@hotmail.com',
-      zip_safe=False,
-      packages=['bioencoder'],
-      include_package_data=True,
-      package_data={
+from setuptools import setup, find_packages
+
+setup(
+    name='bioencoder',
+    version='1.0.0',
+    description='A tool to encode AA to calculable encodings',
+    author='iceshadows',
+    author_email='adron_iceshadows@hotmail.com',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
         "": [".data/*.txt"]
-      },
-      license="AGPL"
-      )
+    },
+    license="AGPL",
+    install_requires=[
+        'numpy'
+    ]
+)
